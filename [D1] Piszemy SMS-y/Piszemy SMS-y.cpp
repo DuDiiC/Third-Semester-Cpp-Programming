cf14565593d2a3f446704d6736394ace3151a121
Maciej Dudek <maciejdudek96@wp.pl>
2018-11-15 08:56:01 +0100

    Add input/output operations

---
+++ [D1] Piszemy SMS-y/Piszemy SMS-y.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int numberOfWords;
+    string word;
+
+    while(cin >> numberOfWords) {
+
+        // create Trie and root
+
+        for(int i = 0; i < numberOfWords; i++) {
+            cin >> word;
+            // add word to Trie
+        }
+
+        cout.precision(2);
+        cout << /* result << */ endl;
+    }
+
+    return 0;
+}
