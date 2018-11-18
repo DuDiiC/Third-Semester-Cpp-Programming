f30111ca6813ed9a590d5cb2612448de3d1c9ffd
Maciej Dudek <maciejdudek96@wp.pl>
2018-11-18 19:13:21 +0100

    Delete unnecessary variables
    Variables: letterInAlphabet and numberOfRules

--- [D2] Porzadkowanie/Porzadkowanie.cpp
+++ [D2] Porzadkowanie/Porzadkowanie.cpp
@@ -16,13 +16,11 @@ int main() {
         outOfLine = "\n";
         vector < char > letters;
         vector < pair < char, char > > rules;
-        int letterInAlphabet, numberOfRules;
 
         for(int i = 0; i < line.size(); i += 2) {
             letters.push_back(line[i]);
         }
 
-        letterInAlphabet = letters.size();
         sort(letters.begin(), letters.end());
 
         getline(cin, lineWithRules);
@@ -30,8 +28,6 @@ int main() {
         for(int i = 0; i < lineWithRules.size(); i += 4) {
             rules.push_back(make_pair(lineWithRules[i], lineWithRules[i+2]));
         }
-
-        numberOfRules = rules.size();
 
         do {
             if(goodPermutation(letters, rules)) {
