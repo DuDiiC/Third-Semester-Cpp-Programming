6b25adcc182c68d2e5f837b18d3228e3dff4260a
Maciej Dudek <maciejdudek96@wp.pl>
2018-12-04 07:34:57 +0100

    Add main function implementation

--- [G1] Wyznacznik/Wyznacznik.cpp
+++ [G1] Wyznacznik/Wyznacznik.cpp
@@ -4,5 +4,34 @@
 using namespace std;
 
 int main() {
+
+    int mSize;
+    cin >> mSize;
+    while(mSize) {
+        SquareMatrix < int > m(mSize);
+        for(int i = 0; i < mSize; i++) {
+            vector < int > row;
+            for(int j = 0; j < mSize; j++) {
+                int temp;
+                cin >> temp;
+                row.push_back(temp);
+            }
+            m.matrix.push_back(row);
+
+        }
+
+        m.printMatrix();
+        cout << endl;
+        m.multiplyColXByY(1, 2);
+        m2.printMatrix();
+        cout << endl;
+
+        cout << m.determinant() << endl;
+
+        cin >> mSize;
+    }
+
+    cout << "*" << endl;
+
     return 0;
 }
