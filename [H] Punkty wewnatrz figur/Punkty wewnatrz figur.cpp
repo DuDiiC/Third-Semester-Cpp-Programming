1867eddb0bcef3281f10583d6dbd2fc78de431dc
Maciej Dudek <maciejdudek96@wp.pl>
2018-12-11 19:09:13 +0100

    Add class Point and operaotors << and >> for Point

--- [H] Punkty wewnatrz figur/Punkty wewnatrz figur.cpp
+++ [H] Punkty wewnatrz figur/Punkty wewnatrz figur.cpp
@@ -3,6 +3,36 @@
 #include <cmath>
 using namespace std;
 
+class Point {
+public:
+    double x;
+    double y;
+
+    Point(){};
+
+    Point(double x, double y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    friend istream& operator>>(istream &input, Point &p);
+    friend ostream& operator<<(ostream &output, const Point &p);
+};
+
+istream& operator>>(istream &input, Point &p) {
+    double x, y;
+    input >> x >> y;
+    p.x = x;
+    p.y = y;
+
+    return input;
+}
+
+ostream& operator<<(ostream &output, const Point &p) {
+    output << "(" << p.x << "; " << p.y << ")";
+    return output;
+}
+
 int main() {
 
 
