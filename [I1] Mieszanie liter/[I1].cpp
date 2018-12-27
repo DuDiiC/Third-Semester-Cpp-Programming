de2600dcf233df8f0d42edb45d8c26bfce93ae82
Maciej Dudek <maciejdudek96@wp.pl>
2018-12-27 14:06:41 +0100

    Add Word class

--- [I1] Mieszanie liter/[I1].cpp
+++ [I1] Mieszanie liter/[I1].cpp
@@ -3,6 +3,18 @@
 #include <vector>
 using namespace std;
 
+class Word {
+public:
+    string word;
+
+    Word() {}
+    Word(string w);
+
+    friend bool operator==(const Word &w1, const Word &w2);
+    friend ostream& operator<<(ostream &output, const Word &w);
+    friend istream& operator>>(istream &input, Word &w);
+};
+
 int main() {
 
     return 0;
