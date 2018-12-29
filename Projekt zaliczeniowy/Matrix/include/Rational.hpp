3f8953ea8456782eff8a6bb397967e5ca01d1fb7
Maciej Dudek <maciejdudek96@wp.pl>
2018-12-29 11:15:13 +0100

    Add base of Rational class

--- Projekt zaliczeniowy/Matrix/include/Rational.hpp
+++ Projekt zaliczeniowy/Matrix/include/Rational.hpp
@@ -9,6 +9,48 @@
 
 class Rational
 {
+public:
+    // constructor
+	Rational();
+	Rational(long long numerator, long long denominator = 1);
+	Rational(const Rational &r);
+
+	// getters
+	inline long long getNumerator();
+	inline long long getDenominator();
+
+	// setters
+	inline void setNumerator(long long n);
+	inline void setDenominator(long long d);
+
+	// overloading arithmetic operators
+	Rational operator+(Rational &r);
+	Rational operator-(Rational &r);
+	Rational operator*(Rational &r);
+	Rational operator/(Rational &r);
+
+    // exponentation
+	Rational exp(long long exponent);
+
+	// overloading relational operators
+	friend bool operator==(const Rational &r1, const Rational &r2);
+	friend bool operator<(const Rational &r1, const Rational &r2);
+	friend bool operator>(const Rational &r1, const Rational &r2);
+	friend bool operator<=(const Rational &r1, const Rational &r2);
+	friend bool operator>=(const Rational &r1, const Rational &r2);
+
+	// overliading operators << and >>
+	friend ostream& operator<<(ostream &output, const Rational &r);
+	friend istream& operator>>(istream &input, Rational &r);
+
+	void saveAsIrreducible();
+
+private:
+	long long numerator;
+	long long denominator;
+
+	static long long gcd(long long x, long long y); // NWD
+	static long long lcm(long long x, long long y); // NWW
 
 };
 
